a0cafb5ebc872e4b44538adff4ecd6a4e8e6fae0
Dan Hopper <dhopper2@sbcglobal.net>
2018-10-03 16:31:04 -0500

    30

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -102,7 +102,7 @@ router.post("/updateAppointment", function(req, res) {
           typeOfAppointment: req.body.typeOfAppointment,
           startDate: req.body.startDate,
           endDate: req.body.endDate,
-          user_id: req.body.id
+          User_id: req.body.id
         });
         res.json("")
     }
