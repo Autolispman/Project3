9d86b0b07205a950edfbb4d193f83f0b8706ccb3
Dan Hopper <dhopper2@sbcglobal.net>
2018-10-26 14:47:38 -0500

    Added New Client button

---
+++ client/src/components/Items/NewUserButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewUserButton = props => (
+    <a className="btn waves-effect waves-light" onClick={props.newClient}>New Client</a>
+);
+
+export default NewUserButton;
