b1146e7ceb43ea1ecd323d8bb29f36d136485777
Dan Hopper <dhopper2@sbcglobal.net>
2018-10-27 15:25:05 -0500

    Added new pet button

---
+++ client/src/components/Items/PetItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PetItem = props => (
+    <div>
+    <button>+</button>
+    <span>Rover</span>
+    </div>
+    // <a className="btn waves-effect waves-light" data-firstname={props.firstName} data-lastname={props.lastName}onClick={props.editClient}>{props.lastName}, {props.firstName}</a>
+);
+
+export default PetItem;
