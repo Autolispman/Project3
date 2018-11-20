ba94ba4d23ce588652d4f58d2cbcba9fed7d8760
Dan Hopper <dhopper2@sbcglobal.net>
2018-11-19 19:31:38 -0600

    Added pets to Appointment page

--- client/src/components/Items/PetItem.js
+++ client/src/components/Items/PetItem.js
@@ -3,19 +3,31 @@ import React, { Component } from "react";
 class PetItem extends Component {
   state = {
     showDetails: false,
-    plusOrMinus: "+"
+    plusOrMinus: "+",
+    checked: false
   };
 
   toggleShowDetails = () => {
-    this.setState(({
-      showDetails: !this.state.showDetails
-    }), () => this.state.showDetails? this.setState({plusOrMinus: "-"}) : this.setState({plusOrMinus: "+"}))
-  }
+    this.setState(
+      {
+        showDetails: !this.state.showDetails
+      },
+      () =>
+        this.state.showDetails
+          ? this.setState({ plusOrMinus: "-" })
+          : this.setState({ plusOrMinus: "+" })
+    );
+  };
 
   render() {
     return (
       <div>
-        <button onClick={this.toggleShowDetails}>{this.state.plusOrMinus}</button>
+        <div>
+          {this.props.showButtons ? (
+            <div>
+        <button onClick={this.toggleShowDetails}>
+          {this.state.plusOrMinus}
+        </button>
         <span>{this.props.petName}</span>
         <button
           type="text"
@@ -35,22 +47,33 @@ class PetItem extends Component {
         >
           Delete Pet
         </button>
-        {this.state.showDetails ?
-           <div>
-             <ul>
-             <li>Breed: {this.props.breed}</li>
-             <li>Birthday: {this.props.birthday}</li>
-             <li>Gender: {this.props.gender}</li>
-             <li>Fixed: {this.props.fixed}</li>
-             <li>Crate Trained: {this.props.crateTrained}</li>
-             <li>House Trained: {this.props.houseTrained}</li>
-             <li>Feeding Instructions: {this.props.feedingInstructions}</li>
-             <li>Medications: {this.props.medications}</li>
-             <li>Health Issues: {this.props.healthIssues}</li>
-             <li>Notes: {this.props.notes}</li>
-             </ul>
-              </div>
-               : null}
+        
+          
+            {this.state.showDetails ? (
+              <ul>
+                <li>Breed: {this.props.breed}</li>
+                <li>Birthday: {this.props.birthday}</li>
+                <li>Gender: {this.props.gender}</li>
+                <li>Fixed: {this.props.fixed}</li>
+                <li>Crate Trained: {this.props.crateTrained}</li>
+                <li>House Trained: {this.props.houseTrained}</li>
+                <li>Feeding Instructions: {this.props.feedingInstructions}</li>
+                <li>Medications: {this.props.medications}</li>
+                <li>Health Issues: {this.props.healthIssues}</li>
+                <li>Notes: {this.props.notes}</li>
+              </ul>
+            ) : null}
+          </div>
+          ) : null}
+        </div>
+        {this.props.showCheckbox ? (
+          <div>
+            <label>
+              <input type="checkbox" name="pet" />
+              <span>{this.props.petName}</span>
+            </label>
+          </div>
+        ) : null}
       </div>
       // <a className="btn waves-effect waves-light" data-firstname={props.firstName} data-lastname={props.lastName}onClick={props.editClient}>{props.lastName}, {props.firstName}</a>
     );
